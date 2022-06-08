636562e317635e3f9b240e81bbc8fe58353c0ed0
Leozzin075 <99297345+Leozzin075@users.noreply.github.com>
2022-06-07 21:21:24 -0300

    Add files via upload
    Atividade para ser complementada

---
+++ projetoAula/index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sistema de Gestão Escolar</title>
+    <link rel="icon" href="_images/logo.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="_css/login.css">
+  </head>
+  <body class="text-center">
+    <form class="form-signin">
+      <img class="mb-4" src="_images/logo.png" alt="Tela de Login" width="100px" height="100px">
+      <h1 class="h3 mb-3 font-weight-normal">Realize o Login</h1>
+
+      <label for="inputEmail" class="sr-only">Endereço de Email</label>
+      <input type="email" id="inputEmail" class="form-control" placeholder="Digite seu E-mail" required autofocus>
+
+      <label for="inputPassword" class="sr-only">Senha</label>
+      <input type="password" id="inputPassword" class="form-control" placeholder="Digite sua Senha" required><br>
+
+      <button class="btn btn-lg btn-primary" type="submit">Login</button><br>
+
+      <p class="mt-5 mb-3 text-muted">&copy;2022</p>
+    </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
